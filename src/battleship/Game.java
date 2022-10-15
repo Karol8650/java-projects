d2e863b35d12adb7b735f386d2615325d58059fc
Karol8650 <71384877+Karol8650@users.noreply.github.com>
2022-10-16 00:05:40 +0200

    Update Game.java

--- src/battleship/Game.java
+++ src/battleship/Game.java
@@ -1,36 +1,58 @@
 package battleship;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 public class Game {
-    private final Board initialBoard;
-    private final Board battleBoard;
+    private final Player player1;
+    private final Player player2;
     private final Scanner scanner;
-    private final ArrayList<HashSet<String>> coordsOfShips;
-    private int sunkShips;
 
     public Game() {
-        initialBoard = new Board();
-        battleBoard = new Board();
+        player1 = new Player();
+        player2 = new Player();
         scanner = new Scanner(System.in);
-        coordsOfShips = new ArrayList<>();
-        sunkShips = 0;
     }
 
-    void play() {
-        placeShips();
-        System.out.println("The game starts!\n");
-        battleBoard.printBoard();
-        takeAShot();
+    void play() throws IOException {
+        System.out.println("Player 1, place your ships on the game field\n");
+        placeShips(player1);
+        System.out.println("Press Enter and pass the move to another player\n");
+
+        System.in.read();
+
+        System.out.println("Player 2, place your ships on the game field\n");
+        placeShips(player2);
+
+        int numOfShips = Ship.numOfShips();
+        while (true) {
+            System.out.println("Press Enter and pass the move to another player\n");
+
+            System.in.read();
+
+            if (player1.numOfSunkenShips() < numOfShips) {
+                Board.printHiddenBoard();
+                takeAShot(player1, player2, "player1");
+            } else break;
+
+            System.out.println("Press Enter and pass the move to another player\n");
+
+            System.in.read();
+
+            if (player2.numOfSunkenShips() < numOfShips) {
+                Board.printHiddenBoard();
+                takeAShot(player2, player1, "player2");
+            } else break;
+        }
     }
 
-     private void placeShips() {
+    private void placeShips(Player player) {
         String input;
         boolean properInput;
+
+        player.printBoard();
 
         for (Ship ship : Ship.values()) {
             properInput = false;
@@ -41,81 +63,67 @@ public class Game {
             do {
                 input = scanner.nextLine();
                 try {
-                    properInput = checkInput(input, ship);
+                    properInput = checkInput(input, ship, player);
                 } catch (IllegalArgumentException e) {
                     System.out.println(e.getMessage());
                 }
             } while (!properInput);
 
-            placeShip(input, ship.getCells());
-            initialBoard.printBoard();
+            placeShip(input, ship.getCells(), player);
+            player.printBoard();
         }
     }
 
-     private void takeAShot() {
+    private void takeAShot(Player player1, Player player2, String name) {
         String input;
         boolean properInput;
-        final int numOfShips = Ship.numOfShips();
-        System.out.println(("Take a shot!\n"));
-        while(sunkShips < numOfShips) {
-            do {
-                properInput = false;
-                input = scanner.nextLine();
-                try {
-                    properInput = checkCoords(input);
-                } catch (IllegalArgumentException e) {
-                    System.out.println(e.getMessage());
-                }
-            } while (!properInput);
+        player1.printBoard();
+        System.out.println(name + ", it's your turn:\n");
 
-            markAShot(input);
-        }
+        do {
+            properInput = false;
+            input = scanner.nextLine();
+            try {
+                properInput = checkCoords(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!properInput);
 
-        System.out.println("You sank the last ship. You won. Congratulations!");
-
+        markAShot(input, player2);
     }
 
-    private  void markAShot(String input) {
+    private void markAShot(String input, Player player) {
         String[] tokens = retrieveTokens(input);
         int x = tokens[0].charAt(0) - 'A';
         int y = Integer.parseInt(tokens[0].substring(1)) - 1;
         final int numOfShips = Ship.numOfShips();
-        if (initialBoard.getChar(x, y) == 'O') {
-            battleBoard.setChar(x, y, 'X');
-            battleBoard.printBoard();
-            if(checkIfSunk(x, y)) {
-                sunkShips++;
-                if(sunkShips < numOfShips) {
-                    System.out.println("You sank a ship! Specify a new target:\n");
+        if (player.getChar(x, y) == 'O') {
+            player.setChar(x, y, 'X');
+            if (checkIfSunken(x, y, player)) {
+                player.sinkShip();
+                if (player.numOfSunkenShips() < numOfShips) {
+                    System.out.println("You sank a ship!\n");
                 }
+            } else {
+                System.out.println("You hit a ship!\n");
             }
-            else {
-                System.out.println("You hit a ship! Try again:\n");
-            }
-        } else if (initialBoard.getChar(x, y) == '~'){
-            battleBoard.setChar(x, y, 'M');
-            battleBoard.printBoard();
-            System.out.println("You missed! Try again:\n");
+        } else if (player.getChar(x, y) == '~') {
+            player.setChar(x, y, 'M');
+            System.out.println("You missed!\n");
+        } else if (player.getChar(x, y) == 'M') {
+            System.out.println("You missed!\n");
+        } else if (player.getChar(x, y) == 'X') {
+            System.out.println("You hit a ship!\n");
         }
     }
 
-    private boolean checkIfSunk(int x, int y) {
-        String coords = "" + x + y;
-        for(HashSet<String> set: coordsOfShips) {
-            if (set.contains(coords)) {
-                set.remove(coords);
-                if(set.isEmpty()) {
-                    coordsOfShips.remove(set);
-                    return true;
-                }
-                return false;
-            }
-        }
-        return false;
+    private boolean checkIfSunken(int x, int y, Player player) {
+        return player.checkIfSunken(x, y);
     }
 
 
-    private  boolean checkCoords(String input) {
+    private boolean checkCoords(String input) {
         Pattern pattern = Pattern.compile("[A-J](\\d|10)$");
         boolean result = pattern.matcher(input).matches();
         if (!result) {
@@ -124,7 +132,7 @@ public class Game {
         return true;
     }
 
-     private boolean checkInput(String input, Ship ship) throws IllegalArgumentException {
+    private boolean checkInput(String input, Ship ship, Player player) throws IllegalArgumentException {
         if (input == null || ship == null) {
             return false;
         }
@@ -143,28 +151,28 @@ public class Game {
             throw new IllegalArgumentException("Error! Wrong length of the " + ship.getName() + "! Try again:");
         }
 
-        if (!checkCross(coordinates, ship.getCells())) {
+        if (!checkCross(coordinates, ship.getCells(), player)) {
             throw new IllegalArgumentException("Error! Ships crossed each other! Try again:");
         }
 
-        if (!checkTouch(coordinates, ship.getCells())) {
+        if (!checkTouch(coordinates, ship.getCells(), player)) {
             throw new IllegalArgumentException("Error! You placed it too close to another one. Try again:");
         }
 
         return true;
     }
 
-    private  boolean checkFormat(String input) {
+    private boolean checkFormat(String input) {
         Pattern pattern = Pattern.compile("[A-J](\\d|10)\\s[A-J](\\d|10)$");
         return pattern.matcher(input).matches();
     }
 
-     private boolean checkLocation(Coordinates coordinates) {
+    private boolean checkLocation(Coordinates coordinates) {
         return (coordinates.getX1() == coordinates.getX2() || coordinates.getY1() == coordinates.getY2()) &&
                 ((coordinates.getX1() != coordinates.getX2() || coordinates.getY1() != coordinates.getY2()));
     }
 
-     private boolean checkLength(Coordinates coordinates, int cells) {
+    private boolean checkLength(Coordinates coordinates, int cells) {
         if (coordinates.getX1() == coordinates.getX2()) {
             if (Math.abs((coordinates.getY1() - coordinates.getY2())) == cells - 1) {
                 return true;
@@ -177,21 +185,21 @@ public class Game {
         return false;
     }
 
-     private boolean checkCross(Coordinates coordinates, int cells) {
+    private boolean checkCross(Coordinates coordinates, int cells, Player player) {
         int row;
         int col;
         if (coordinates.getX1() == coordinates.getX2()) {
             col = Math.min(coordinates.getY1(), coordinates.getY2());
             row = coordinates.getX1();
             for (int i = col; i < col + cells; i++) {
-                if (initialBoard.getChar(row, i) == 'O')
+                if (player.getChar(row, i) == 'O')
                     return false;
             }
         } else {
             row = Math.min(coordinates.getX1(), coordinates.getX2());
             col = coordinates.getY1();
             for (int i = row; i < row + cells; i++) {
-                if (initialBoard.getChar(i, col) == 'O')
+                if (player.getChar(i, col) == 'O')
                     return false;
             }
         }
@@ -199,21 +207,21 @@ public class Game {
         return true;
     }
 
-     private boolean checkTouch(Coordinates coordinates, int cells) {
+    private boolean checkTouch(Coordinates coordinates, int cells, Player player) {
         int row;
         int col;
         if (coordinates.getX1() == coordinates.getX2()) {
             col = Math.min(coordinates.getY1(), coordinates.getY2());
             row = coordinates.getX1();
             for (int i = col; i < col + cells; i++) {
-                if (!checkField(row, i))
+                if (!checkField(row, i, player))
                     return false;
             }
         } else {
             row = Math.min(coordinates.getX1(), coordinates.getX2());
             col = coordinates.getY1();
             for (int i = row; i < row + cells; i++) {
-                if (!checkField(i, col))
+                if (!checkField(i, col, player))
                     return false;
             }
         }
@@ -235,46 +243,45 @@ public class Game {
         StringTokenizer tokenizer = new StringTokenizer(input);
         final int size = tokenizer.countTokens();
         String[] tokens = new String[size];
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             tokens[i] = tokenizer.nextToken();
         }
         return tokens;
     }
 
-     private void placeShip(String input, int cells) {
+    private void placeShip(String input, int cells, Player player) {
         Coordinates coordinates = retrieveCoordinates(input);
 
         int row;
         int col;
-        coordsOfShips.add(new HashSet<>());
-        int size = coordsOfShips.size();
+        player.addShip();
         if (coordinates.getX1() == coordinates.getX2()) {
             col = Math.min(coordinates.getY1(), coordinates.getY2());
             row = coordinates.getX1();
             for (int i = col; i < col + cells; i++) {
-                initialBoard.setChar(row, i, 'O');
+                player.setChar(row, i, 'O');
                 String coords = "" + row + i;
-                coordsOfShips.get(size - 1).add(coords);
+                player.addCoordsToLastShip(coords);
             }
         } else {
             row = Math.min(coordinates.getX1(), coordinates.getX2());
             col = coordinates.getY1();
             for (int i = row; i < row + cells; i++) {
-                initialBoard.setChar(i, col, 'O');
+                player.setChar(i, col, 'O');
                 String coords = "" + i + col;
-                coordsOfShips.get(size - 1).add(coords);
+                player.addCoordsToLastShip(coords);
             }
         }
     }
 
-     private boolean checkField(int row, int col) {
+    private boolean checkField(int row, int col, Player player) {
         final int size = 8;
         int[][] coords = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}};
 
         for (int i = 0; i < size; i++) {
             if (row + coords[i][0] >= 0 && row + coords[i][0] <= 9 &&
                     col + coords[i][1] >= 0 && col + coords[i][1] <= 9 &&
-                    initialBoard.getChar(row + coords[i][0], col + coords[i][1]) == 'O') {
+                    player.getChar(row + coords[i][0], col + coords[i][1]) == 'O') {
                 return false;
             }
         }
